deeb3fcd62d6753aeecfeb20908b5ff4e7449742
fuxuewei <931865616@qq.com>
2017-07-10 09:59:16 +0800

    修改

--- app/src/main/java/com/baway/fuxuewei/fuxuewei20170710/MainActivity.java
+++ app/src/main/java/com/baway/fuxuewei/fuxuewei20170710/MainActivity.java
@@ -2,12 +2,32 @@ package com.baway.fuxuewei.fuxuewei20170710;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private AmountView mAmountView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mAmountView = (AmountView) findViewById(R.id.amount_view);
+        mAmountView.setGoods_storage(50);
+        mAmountView.setOnAmountChangeListener(new AmountView.OnAmountChangeListener() {
+            @Override
+            public void onAmountChange(View view, int amount) {
+                Toast.makeText(getApplicationContext(), "Amount=>  " + amount, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
+
+
+
+
 }
